9587a8fceedb70a7681d22535bea7cd30d0c9257
Anton Tigunov <seyanaracore@hotmail.com>
2025-04-22 23:16:45 +0300

    feat(app): add controller error message to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import cls from './app.module.scss'
 import { isProdService } from './consts'
+import { useQueryController } from './query'
 import ModuleSelect from '@/components/ModuleSelect'
 import ScreenSize from '@/components/ScreenSize'
 import CalculatorTable from '@/components/CalculatorTable'
@@ -10,6 +11,7 @@ import { useCartSummaryPrice, useFinishedProductPrice } from '@/hooks/useCartPri
 const App = () => {
   const finishedProductPrice = useFinishedProductPrice()
   const summaryPrice = useCartSummaryPrice()
+  const { isError: controllerIsError } = useQueryController()
 
   return (
     <div class={cls.root}>
@@ -18,6 +20,14 @@ const App = () => {
         <ScreenSize />
         <ImplementationTypeSelect />
       </div>
+      {controllerIsError && (
+        <p
+          style="margin-bottom: 0; margin-top: 1rem"
+          className="red_text"
+        >
+          Ошибка загрузки данных. Не удалось определить контроллер для заданных парамеров
+        </p>
+      )}
       <CalculatorTable />
       <div className={cls.tableSummaryContainer}>
         {!isProdService && (
