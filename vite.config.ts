03c3a8196347039d9deaddbe9d0aa09ba4a1be21
Anton Tigunov <seyanaracore@hotmail.com>
2025-04-22 23:17:29 +0300

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,73 @@
 import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { preact } from '@preact/preset-vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { viteExternalsPlugin } from 'vite-plugin-externals'
+import { defineConfig } from 'vite'
+import type { UserConfig } from 'vite'
 
 const LED_EXPRESS_URL = 'https://ledexpress.ru'
-const isBuild = !!(process.env.NODE_ENV === 'production' && process.env.lib)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+function getViteConfig(env: { ANALYZER?: boolean } & typeof process.env): UserConfig {
+  const analyzer = env.ANALYZER ?? false
+
+  const plugins: UserConfig['plugins'] = [
     preact(),
     viteExternalsPlugin({
       jquery: 'jQuery',
     }),
-  ],
-  server: {
-    cors: false,
-    proxy: {
-      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-      '/upage': {
-        secure: false,
-        target: LED_EXPRESS_URL,
-        changeOrigin: true,
-      },
-    },
-  },
-  resolve: {
-    alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use "@/assets/scss/variables.scss" as *;
-          @use "@/assets/scss/mixins" as *;
-        `,
-      },
-    },
-  },
-  build: isBuild
-    ? {
-        target: 'es6',
-        lib: {
-          // Could also be a dictionary or array of multiple entry points
-          entry: './src/index.tsx',
-          name: 'calc',
-          // the proper extensions will be added
-          fileName: 'form-calculation',
+  ]
+
+  if (analyzer) {
+    plugins.push(
+      visualizer({
+        open: true,
+      })
+    )
+  }
+
+  return {
+    plugins,
+    server: {
+      cors: false,
+      proxy: {
+        // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+        '/upage': {
+          secure: false,
+          target: LED_EXPRESS_URL,
+          changeOrigin: true,
         },
-      }
-    : undefined,
-})
+      },
+    },
+    resolve: {
+      alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/scss/variables.scss" as *;
+            @use "@/assets/scss/mixins" as *;
+          `,
+        },
+      },
+    },
+    build: {
+      target: 'es6',
+      cssCodeSplit: true,
+      emptyOutDir: true,
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: './src/index.tsx',
+        name: 'calc',
+        // the proper extensions will be added
+        fileName: 'form-calculation',
+      },
+    },
+  }
+}
+
+export default defineConfig(getViteConfig(process.env))
